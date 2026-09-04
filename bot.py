45645cfe4c9115e0cdf3a1fd125cb7c24cad369d
deniskomar5533-droid <deniskomar5533@gmail.com>
2026-09-04 22:10:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -435,3 +435,16 @@ def callback_handler(call):
 
 
 bot.infinity_polling(skip_pending=True)
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
